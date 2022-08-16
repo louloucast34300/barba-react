d69601f85e0c8644d9be7a14780506ea21659542
LOUIS CASTEL <louis@glanum.com>
2022-08-16 17:00:41 +0200

    feat(accueil) test-scrollTrigger

--- src/page/Accueil.jsx
+++ src/page/Accueil.jsx
@@ -4,13 +4,20 @@ import { motion } from "framer-motion";
 import "../css/base.css";
 import Title from "../component/title/Title";
 import { Donnuts } from "../component/donnuts/Donnuts";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+  gsap.registerPlugin(ScrollTrigger);
 
 export const Accueil = ({ transitionColor, currentColorPage }) => {
+    
+
+ //Animation d'entrée
   let screen = useRef(null);
   let body = useRef(null);
-
   useEffect(() => {
+
+    console.log( gsap.registerPlugin(ScrollTrigger));
     const tl = gsap.timeline();
+    
     const colorPage = screen.style.backgroundColor;
     tl.to(screen, {
       duration: 1,
@@ -18,43 +25,68 @@ export const Accueil = ({ transitionColor, currentColorPage }) => {
       width: "250vw",
       height: "250vh",
       borderRadius: "100%",
-      ease: "Power4.easeOut",
+      ease: "easeInOut",
     });
     return () => {
-      tl.to(screen,{
+      tl.to(
+        screen,
+        {
           display: "none",
           duration: 0.1,
-        },"-=.7");
-      tl.to(body,{
+        },
+        "-=.7"
+      );
+      tl.to(
+        body,
+        {
           opacity: 1,
           duration: 0.1,
           backgroundColor: colorPage,
-        },"-=.7");
+        },
+        "-=.7"
+      );
+    
     };
-  });
+  },[]);
+
+  //Scroll Trigger
+  
+  let textAnim = useRef(null);
+  let cubeAnim = useRef(null);
+  useEffect(()=>{
+    gsap.to(textAnim, {rotation:180, duration :3,delay:4, scrollTrigger:{
+      trigger: textAnim,
+    
+    }})
+    gsap.fromTo(cubeAnim, {x:-200, rotation:0}, {x: 200, rotation: 360, duration:2, ease: "Power4.easeOut", scrollTrigger:{
+      trigger: cubeAnim,
+      toggleActions : 'restart none reverse none'
+    }})
+  },[])
 
   return (
     <>
       <div className="load-container">
-        <div
-          className="load-screen"
-          ref={(el) => (screen = el)}
-          style={{ backgroundColor: transitionColor }}
-        ></div>
+        <div className="load-screen" ref={(el) => (screen = el)} style={{ backgroundColor: transitionColor }}></div>
       </div>
       <div className="Home" style={{ backgroundColor: currentColorPage }}>
         <div ref={(el) => (body = el)} className="Headd">
           <div className="container">
-            <Title 
-            title={`Nous connectons des entrepreneurs brillants à des investisseurs expérimentés`} 
-            speed={10} 
-            count={3}/> 
-            <Donnuts style='vert'/>
-            <h1>coucou</h1>
+            <Title
+              title={`Nous connectons des entrepreneurs brillants à des investisseurs expérimentés`}
+              speed={10}
+              count={3}
+            />
+            <Donnuts style="vert" />
+            <div style={{height:'30vh'}}></div>
+            <div style={{height:'100vh', display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'space-around', backgroundColor:'white'}}>
+              <h1 className='trigger-text' ref={(el)=>(textAnim = el)} style={{color:'white'}}>Test</h1>
+              <div ref={(el) => (cubeAnim = el)} style={{backgroundColor:'red', height:'300px',width:"300px"}}></div>
+            </div>
+            <div style={{height:'300vh'}}></div>
           </div>
         </div>
       </div>
-     
     </>
   );
 };
